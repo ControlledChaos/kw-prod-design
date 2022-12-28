6f05b1896411b44ff04a14df34ed51f188f190d3
ControlledChaos <greg@ccdzine.com>
2022-12-27 20:15:29 -0800

    External/IMDb link icon.

--- views/frontend/content/content-single-project-acf.php
+++ views/frontend/content/content-single-project-acf.php
@@ -10,6 +10,8 @@
 
 use KWProd\Front as Front;
 
+$icon_link = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M497.6,0,334.4.17A14.4,14.4,0,0,0,320,14.57V47.88a14.4,14.4,0,0,0,14.69,14.4l73.63-2.72,2.06,2.06L131.52,340.49a12,12,0,0,0,0,17l23,23a12,12,0,0,0,17,0L450.38,101.62l2.06,2.06-2.72,73.63A14.4,14.4,0,0,0,464.12,192h33.31a14.4,14.4,0,0,0,14.4-14.4L512,14.4A14.4,14.4,0,0,0,497.6,0ZM432,288H416a16,16,0,0,0-16,16V458a6,6,0,0,1-6,6H54a6,6,0,0,1-6-6V118a6,6,0,0,1,6-6H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V304A16,16,0,0,0,432,288Z"/></svg>';
+
 // Custom fields.
 $title       = get_field( 'project_title' );
 $client      = get_field( 'project_client' );
@@ -95,10 +97,11 @@ if ( ! is_null( $featured ) ) {
 
 if ( $imdb_url ) {
 	$imdb = sprintf(
-		'<p class="project-info-entry project-imdb"><a href="%1s" target="_blank" rel="nofollow">%2s %3s <span class="icon-right"></span></a></p>',
+		'<p class="project-info-entry project-imdb"><a href="%1s" target="_blank" rel="nofollow">%2s %3s <span class="icon icon-link">%s</span></a></p>',
 		esc_url( $imdb_url ),
 		$title,
-		__( 'on IMDb', 'kw-prod-design' )
+		__( 'on IMDb', 'kw-prod-design' ),
+		$icon_link
 	);
 } else {
 	$imdb = null;
